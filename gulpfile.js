11edc377637889aaf06ea719aa3954d398de0987
thekingdomofheaven <dsyoo80@naver.com>
2019-05-20 18:32:15 +0900

    gulp setting

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass',function(){
+  return gulp.src('./src/scss/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default',['sass']);

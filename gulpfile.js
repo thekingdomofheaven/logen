296a14fc353185d2618406b80f1d87f0b1d8ce4a
doosungyoo <dsyoo80@naver.com>
2019-05-21 14:27:53 +0900

    htmlcopy function

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
-const {src, dest} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass');
 
 function stylebuild(){
   return src('./src/scss/**/*.scss')
   .pipe(sass({
-    outputStyle:'compressed'
-  }).on('error', sass.logError))
+		errLogToConsole: true,
+		outputStyle: 'compressed' // nested, expanded, compact, or compressed.
+	}).on('error', sass.logError))
   .pipe(dest('./dist/css'));
 }
 
-exports.default = stylebuild;
+function htmlcopy(){
+  return src('./src/index.html')
+  .pipe(dest('./dist'));
+}
+
+exports.default = function(){
+  watch('./src/scss/**/*.scss',stylebuild);
+  watch('./src/index.html', htmlcopy);
+};
